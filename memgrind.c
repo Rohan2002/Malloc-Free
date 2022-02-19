efd6605a4d672e1f2e67ba27d82328731d6e6568
Rohan Deshpande <rohandeshpande832@gmail.com>
2022-02-19 11:21:30 -0500

    Feat: update codebase with selins free

--- memgrind.c
+++ memgrind.c
@@ -3,5 +3,15 @@
 
 
 int main(){
-    malloc(sizeof(int)*2);
+    char*z = (char*)malloc(sizeof(char));
+    *z='c';
+    printf("%c\n",*z);
+    int *x = (int*)malloc(sizeof(int)*3);
+    int y=5;
+    //free(&y);
+    free(z);
+    char*z2 = (char*)malloc(sizeof(char)*2);
+    free(x+2);
+    free(x);
+    print_heap();
 }
--- mymalloc.c
+++ mymalloc.c
@@ -39,13 +39,14 @@ void *mymalloc(size_t size, char *file, int line){
             return (void *)current;
 
         } else if (payload_size >= size + 1 + sizeof(t_meta_data ) && meta_data_free==1) { // split case
+
             unsigned short part1_chunksize = size + sizeof(t_meta_data );
-            c->data = part1_chunksize ;
+            c->data = part1_chunksize + free_bit*0 ;
 
             unsigned short part2_chunksize = chunk_size -part1_chunksize;
             current += part1_chunksize;
             c = (void*)current;
-            c->data = part2_chunksize + free_bit;
+            c->data = part2_chunksize + free_bit*1;
             current -= part1_chunksize;
             current+=sizeof(t_meta_data );
             return (void *)current;
@@ -74,45 +75,85 @@ void print_heap(){
         unsigned short chunk_size = c->data ;
         unsigned short payload_size = c->data - sizeof(t_meta_data );
         unsigned short meta_data_free=0;
+        if (c->data >= free_bit ) {
+            meta_data_free= 1;
+            payload_size -=free_bit ;
+            chunk_size -=free_bit ;
+        }
 
                 printf("CHUNK #%d\n",number);
         printf("chunk encoded data : %hu\n",c->data);
         printf("chunk_size : %hu\n",chunk_size );
         printf("payload_size : %hu\n",payload_size );
         printf("is free : %hu\n",meta_data_free );
+        printf("current - memory + chunk_size : %d\n",current - memory + chunk_size );
 
             if (  current - memory + chunk_size >=MEM_SIZE ) {
                 current=NULL;
             } else {
                 current+=chunk_size ;
             }
-
     }
 
     printf("-------------------------------------\n");
 }
 
 void myfree(void *ptr, char *file, int line){
-    printf("hello free\n");
         // memory array is not initialized
     // initlaize it with a free chunk of 4096 bytes
     if (memory[0] == 255) {
 
-        char c1,c2;
-        short chunksize = MEM_SIZE;
-        short chunk_allocation = 0;
-        short meta_data = chunksize + 32768*chunk_allocation;
-        memory[0]=meta_data >>8;
-        memory[1]=meta_data % 256;
-        for (int i=2;i<MEM_SIZE; i++){
-            memory[i]=0;
+     //error
+    }
+    if (ptr <memory || ptr>memory+MEM_SIZE) {
+        printf("Error: trying to free an invalid address.\n");
+        printf("Error in %s at line %d\n",file,line);
+        return;
+    }
+    t_meta_data *meta_data_header = (void*)memory;
+    unsigned short free_bit = 1 << 15;
+
+
+    char *current = memory;
+    while (current!=NULL) {
+        if (current > ptr) {
+            printf("Error: trying to free an invalid address.\n");
+            printf("Error in %s at line %d\n",file,line);
+            return;
+        }
+         t_meta_data *c = (void*)current;
+         unsigned short chunk_size = c->data ;
+         unsigned short payload_size = c->data - sizeof(t_meta_data );
+        unsigned short meta_data_free=0;
+        if (c->data >= free_bit ) {
+            meta_data_free= 1;
+            payload_size -=free_bit ;
+            chunk_size -=free_bit ;
+        }
+        if (current + sizeof(t_meta_data ) == ptr) {
+
+            if (meta_data_free==0) {
+                c->data += free_bit;
+                return;
+            } else {
+                printf("Error: trying to free an already freed address.\n");
+                printf("Error in %s at line %d\n",file,line);
+                return;
+            }
+        }
+
+
+       if (  current - memory + chunk_size >=MEM_SIZE ) {
+            current=NULL;
+        } else {
+            current+=chunk_size ;
         }
     }
-}
+    if (current > ptr) {
+            printf("Error: trying to free an invalid address.\n");
+            printf("Error in %s at line %d\n",file,line);
+            return;
+    }
 
-int main(){
-    print_heap();
-    mymalloc(20,"df",23);
-    print_heap();
 }
 
