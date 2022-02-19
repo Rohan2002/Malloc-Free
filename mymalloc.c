4db4eaf3b88ae6859e276f1729b30d50d905aded
Rohan Deshpande <rohandeshpande832@gmail.com>
2022-02-19 18:52:08 -0500

    feat: include old/new code

--- mymalloc.c
+++ mymalloc.c
@@ -8,66 +8,131 @@
 //         c2=meta_data % 256;
 // }
 
-void *mymalloc(size_t requested_size, char *file, int line)
+void *old_mymalloc(size_t size, char *file, int line)
 {
-    // free = 1, means its block is freed. free = 0 means its block is allocated.
-    t_meta_data *initial_header = (void *)memory;
+    // printf("hello malloc: %s %d\n",file,line);
+    //  memory array is not initialized
+    //  initlaize it with a free chunk of 4096 bytes
+    s_meta_data *meta_data_header = (void *)memory;
     unsigned short free_bit = 1 << 15;
     if (memory[0] == -1)
     {
-        // Memset here is much cleaner: https://en.cppreference.com/w/cpp/string/byte/memset
-        memset(memory, 0, MEM_SIZE);
-
-        // Init initial_header with payload_size = 4096, free = 1
-        initial_header->block_size = MEM_SIZE;
-        initial_header->free = 1;
+        for (int i = 0; i < MEM_SIZE; i++)
+        {
+            memory[i] = 0;
+        }
+        meta_data_header->data = MEM_SIZE + free_bit;
     }
 
-    char* current_block_pointer = &memory[0]; // base address of memory.
+    char *current = memory;
+    while (current != NULL)
+    {
+        s_meta_data *c = (void *)current;
+        unsigned short chunk_size = c->data;
+        unsigned short payload_size = c->data - sizeof(s_meta_data);
+        unsigned short meta_data_free = 0;
+
+        if (c->data >= free_bit)
+        {
+            meta_data_free = 1;
+            payload_size -= free_bit;
+            chunk_size -= free_bit;
+        }
+        if (payload_size == size && meta_data_free == 1)
+        { // same amount of demand
+            c->data -= free_bit;
+            current += sizeof(s_meta_data);
+            return (void *)current;
+        }
+        else if (payload_size >= size + 1 + sizeof(s_meta_data) && meta_data_free == 1)
+        { // split case
+
+            unsigned short part1_chunksize = size + sizeof(s_meta_data);
+            c->data = part1_chunksize + free_bit * 0;
+
+            unsigned short part2_chunksize = chunk_size - part1_chunksize;
+            current += part1_chunksize;
+            c = (void *)current;
+            printf("selin Address in method of header2 %p\n", c);
+            c->data = part2_chunksize + free_bit * 1;
+            current -= part1_chunksize;
+            current += sizeof(s_meta_data);
+            return (void *)current;
+        }
+        else
+        {
+            if (current - memory + chunk_size >= MEM_SIZE)
+            {
+                current = NULL;
+            }
+            else
+            {
+                current += chunk_size;
+            }
+        }
+    }
+    return NULL;
+}
+
+void *new_mymalloc(size_t requested_size, char *file, int line)
+{
+    if (init == 0)
+    {
+        t_meta_data *initial_header = (void *)memory;
+        if (memory[0] == -1)
+        {
+            // Memset here is much cleaner: https://en.cppreference.com/w/cpp/string/byte/memset
+            memset(memory, 0, MEM_SIZE);
+
+            // Init initial_header with payload_size = 4096, free = 1
+            initial_header->block_size = MEM_SIZE;
+            initial_header->free = 1;
+        }
+        init = 1;
+    }
+
+    void *current_block_pointer = &memory[0]; // base address of memory.
     int index = 0;
 
     while (index < MEM_SIZE)
     {
-        t_meta_data *metadata_of_current_block = (void *)current_block_pointer;
+        t_meta_data *metadata_of_current_block = current_block_pointer;
 
         unsigned short header_size = sizeof(t_meta_data);
-        unsigned short current_payload_size = metadata_of_current_block->block_size - header_size;
         unsigned short requested_block_size = requested_size + header_size; // includes the header size with requested_size.
 
         unsigned short current_block_size = metadata_of_current_block->block_size;
 
-        // same amount of demand 
+        // same amount of demand
         if (metadata_of_current_block->free == 1 && requested_block_size == current_block_size)
         {
             // set metadata to allocated.
-            metadata_of_current_block->free = 0; 
+            metadata_of_current_block->free = 0;
             metadata_of_current_block->block_size = requested_size;
 
-            // push the current_block_pointer to the start of payload (NOT the header) in the block.
             current_block_pointer += header_size;
-
-            return (void *)current_block_pointer;
+            return current_block_pointer;
         }
         // split case
         else if (metadata_of_current_block->free == 1 && requested_block_size < current_block_size)
-        {   
-            
+        {
+
             // current block is split into two parts block1 and block2.
-            // Two headers(header1 and header2) in the free list corrosponding to block1 and block2. 
-            
+            // Two headers(header1 and header2) in the free list corrosponding to block1 and block2.
+
             // block1 and block2
             // |          |
             // header1 -> header2
 
             // Fill header1
             metadata_of_current_block->block_size = requested_block_size;
-            metadata_of_current_block->free = 0;        
+            metadata_of_current_block->free = 0;
 
             /*
                 Compute the address of where header2 will be located
                 because we don't have a t_meta_data "next" attribute.
 
-                The way I thought about this address computation. 
+                The way I thought about this address computation.
                 Let me know what u think Selin.
 
                 current_block
@@ -77,9 +142,9 @@ void *mymalloc(size_t requested_size, char *file, int line)
                     - block2 (free)
                         - header2
                         - payload2
-                
-                metadata_of_current_block points to block1. then 
-                metadata_of_current_block = current_block + block1_size
+
+                metadata_of_current_block points to block1. then
+                metadata_of_current_block = current_block + block1_size = block2_size
 
             */
 
@@ -92,125 +157,123 @@ void *mymalloc(size_t requested_size, char *file, int line)
 
             // push the current_block_pointer to the start of payload (NOT the header) in the block.
             current_block_pointer += header_size;
-            return (void *)current_block_pointer;
+            return current_block_pointer;
         }
         current_block_pointer += current_block_size;
         index += current_block_size;
-        
     }
+    return NULL;
 }
 
-void print_head(){
-
-}
-void print_heap()
+void print_implicit_free_list()
 {
     printf("-------------------------------------\n");
-    t_meta_data *meta_data_header = (void *)memory;
-    unsigned short free_bit = 1 << 15;
-
     char *current = memory;
     int number = 0;
-    while (current != NULL)
+    int index = 0;
+    while (index < MEM_SIZE)
     {
         number++;
         t_meta_data *metadata_of_current_block = (void *)current;
-        unsigned short chunk_size = metadata_of_current_block->data;
-        unsigned short payload_size = metadata_of_current_block->data - sizeof(t_meta_data);
-        unsigned short meta_data_free = 0;
-        if (metadata_of_current_block->data >= free_bit)
-        {
-            meta_data_free = 1;
-            payload_size -= free_bit;
-            chunk_size -= free_bit;
-        }
+        unsigned short chunk_size = metadata_of_current_block->block_size;
+        unsigned short payload_size = chunk_size - sizeof(t_meta_data);
 
         printf("CHUNK #%d\n", number);
-        printf("chunk encoded data : %hu\n", metadata_of_current_block->data);
         printf("chunk_size : %hu\n", chunk_size);
         printf("payload_size : %hu\n", payload_size);
-        printf("is free : %hu\n", meta_data_free);
-        printf("current - memory + chunk_size : %d\n", current - memory + chunk_size);
+        printf("is free : %s\n", metadata_of_current_block->free == 1 ? "yes" : "no");
+        printf("pointer of payload in memory: %p\n", current);
 
-        if (current - memory + chunk_size >= MEM_SIZE)
-        {
-            current = NULL;
-        }
-        else
-        {
-            current += chunk_size;
-        }
+        index += chunk_size;
+        current += chunk_size;
+        printf("-------------------------------------\n");
     }
 
     printf("-------------------------------------\n");
 }
 
-void myfree(void *ptr, char *file, int line)
+// void myfree(void *ptr, char *file, int line)
+// {
+//     // memory array is not initialized
+//     // initlaize it with a free chunk of 4096 bytes
+//     if (memory[0] == 255)
+//     {
+
+//         // error
+//     }
+//     if (ptr < memory || ptr > memory + MEM_SIZE)
+//     {
+//         printf("Error: trying to free an invalid address.\n");
+//         printf("Error in %s at line %d\n", file, line);
+//         return;
+//     }
+//     t_meta_data *meta_data_header = (void *)memory;
+//     unsigned short free_bit = 1 << 15;
+
+//     char *current = memory;
+//     while (current != NULL)
+//     {
+//         if (current > ptr)
+//         {
+//             printf("Error: trying to free an invalid address.\n");
+//             printf("Error in %s at line %d\n", file, line);
+//             return;
+//         }
+//         t_meta_data *metadata_of_current_block = (void *)current;
+//         unsigned short chunk_size = metadata_of_current_block->block_size;
+//         unsigned short payload_size = metadata_of_current_block->block_size - sizeof(t_meta_data);
+//         unsigned short meta_data_free = 0;
+//         if (metadata_of_current_block->block_size >= free_bit)
+//         {
+//             meta_data_free = 1;
+//             payload_size -= free_bit;
+//             chunk_size -= free_bit;
+//         }
+//         if (current + sizeof(t_meta_data) == ptr)
+//         {
+
+//             if (meta_data_free == 0)
+//             {
+//                 metadata_of_current_block->block_size += free_bit;
+//                 return;
+//             }
+//             else
+//             {
+//                 printf("Error: trying to free an already freed address.\n");
+//                 printf("Error in %s at line %d\n", file, line);
+//                 return;
+//             }
+//         }
+
+//         if (current - memory + chunk_size >= MEM_SIZE)
+//         {
+//             current = NULL;
+//         }
+//         else
+//         {
+//             current += chunk_size;
+//         }
+//     }
+//     if (current > ptr)
+//     {
+//         printf("Error: trying to free an invalid address.\n");
+//         printf("Error in %s at line %d\n", file, line);
+//         return;
+//     }
+// }
+int main(int argc, char **argv)
 {
-    // memory array is not initialized
-    // initlaize it with a free chunk of 4096 bytes
-    if (memory[0] == 255)
-    {
+    // char* z = (char*)s_malloc(sizeof(char));
+    // z = "selin";
+    // printf("Here %c\n",z[3]);
 
-        // error
-    }
-    if (ptr < memory || ptr > memory + MEM_SIZE)
-    {
-        printf("Error: trying to free an invalid address.\n");
-        printf("Error in %s at line %d\n", file, line);
-        return;
-    }
-    t_meta_data *meta_data_header = (void *)memory;
-    unsigned short free_bit = 1 << 15;
+    char *z = (char *)o_malloc(sizeof(char));
 
-    char *current = memory;
-    while (current != NULL)
+    if (z == NULL)
     {
-        if (current > ptr)
-        {
-            printf("Error: trying to free an invalid address.\n");
-            printf("Error in %s at line %d\n", file, line);
-            return;
-        }
-        t_meta_data *metadata_of_current_block = (void *)current;
-        unsigned short chunk_size = metadata_of_current_block->data;
-        unsigned short payload_size = metadata_of_current_block->data - sizeof(t_meta_data);
-        unsigned short meta_data_free = 0;
-        if (metadata_of_current_block->data >= free_bit)
-        {
-            meta_data_free = 1;
-            payload_size -= free_bit;
-            chunk_size -= free_bit;
-        }
-        if (current + sizeof(t_meta_data) == ptr)
-        {
-
-            if (meta_data_free == 0)
-            {
-                metadata_of_current_block->data += free_bit;
-                return;
-            }
-            else
-            {
-                printf("Error: trying to free an already freed address.\n");
-                printf("Error in %s at line %d\n", file, line);
-                return;
-            }
-        }
-
-        if (current - memory + chunk_size >= MEM_SIZE)
-        {
-            current = NULL;
-        }
-        else
-        {
-            current += chunk_size;
-        }
+        printf("No memory");
+        return 1;
     }
-    if (current > ptr)
-    {
-        printf("Error: trying to free an invalid address.\n");
-        printf("Error in %s at line %d\n", file, line);
-        return;
-    }
+    printf("Memory's base address: %p\n", &memory[0]);
+    printf("Size %lu and address: %p and value: %d\n", sizeof(z), z, *z);
 }
--- mymalloc.h
+++ mymalloc.h
@@ -2,21 +2,28 @@
 #define MYMALLOC_H_INCLUDED
 #define MEM_SIZE 4096
 
+typedef struct meta_data
+{
+    unsigned short block_size : 13; // block_size = payload_size + header_size
+    unsigned short nothing : 2;     // ignore this for now.
+    unsigned short free : 1;        // free = 1, means its block is freed. free = 0 means its block is allocated.
 
-typedef struct meta_data {
-    unsigned short block_size: 12; // block_size = payload_size + header_size
-    unsigned short nothing : 3;
-    unsigned short free: 1;
-    
 } t_meta_data;
 
-static char memory[MEM_SIZE]={255};
+typedef struct struct_meta_data
+{
+    unsigned short data;
+
+} s_meta_data;
+
+static int init = 0;
+static char memory[MEM_SIZE] = {255};
 
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
-void print_heap();
-#define malloc(s) mymalloc(s, __FILE__, __LINE__)
+void print_implicit_free_list();
+#define o_malloc(s) old_mymalloc(s, __FILE__, __LINE__)
+#define n_malloc(s) new_mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
-
 
 #endif // MYMALLOC_H_INCLUDED
